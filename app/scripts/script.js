3f2cdebd31cf8a489da355f540576ded7d01fa8f
timi15 <vargatimi505@gmail.com>
2025-09-23 21:44:07 +0200

    feat: load data

---
+++ app/scripts/script.js
@@ -0,0 +1,32 @@
+const data = JSON.parse(localStorage.getItem("data"));
+
+const currentUser = localStorage.getItem("currentUser");
+const tasks = data.users.find(user => user.nickname === currentUser).tasks || [];
+
+//console.log(currentUser);
+
+window.onload = () => {
+    const tbody = document.querySelector("#taskManagerTable tbody");
+    tasks.forEach(task => {
+        tbody.insertAdjacentHTML("beforeend", renderTaskRow(task));
+    });
+    console.log(tasks);
+}
+
+const renderTaskRow = (task) => {
+    return `
+        <tr>
+            <td>${task.id}</td>
+            <td>${task.title}</td>
+            <td>${task.description}</td>
+            <td>${task.category}</td>
+            <td>${task.deadline}</td>
+            <td>${task.priority}</td>
+            <td>${task.status}</td>
+            <td>
+                <button class="btn">Szerkesztés</button>
+                <button class="btn">Törlés</button>
+            </td>
+        </tr>
+    `;
+}
